1e0de31a35d4d5283a744b6bda01995220bf229c
devans2718 <daniel.e2718@gmail.com>
2025-03-08 19:53:08 -0600

    sql file to insert assets into database

---
+++ db/insert_assets.sql
@@ -0,0 +1,146 @@
+INSERT INTO
+    assets (name, category, description)
+VALUES
+    (
+        "server",
+        "hardware",
+        "houses e-commerce website"),
+    (
+        "switch",
+        "hardware",
+        "manages internal network traffic"
+    ),
+    (
+        "router",
+        "hardware",
+        "manages internet traffic through the network"
+    ),
+    (
+        "phones",
+        "hardware",
+        "enables calls for sales, service, and other communications"
+    ),
+    (
+        "workstation_owner",
+        "hardware",
+        "increased permissions"
+    ),
+    (
+        "workstation_it",
+        "hardware",
+        "most permissions"),
+    (
+        "workstation_csr",
+        "hardware",
+        "standard permissions"
+    ),
+    (
+        "workstation_csr",
+        "hardware",
+        "standard permissions"
+    ),
+    (
+        "workstation_sales",
+        "hardware",
+        "standard permissions"
+    ),
+    (
+        "workstation_inv",
+        "hardware",
+        "inventory management access only"
+    ),
+    (
+        "ims",
+        "software",
+        "inventory management system"
+    ),
+    (
+        "cms",
+        "software",
+        "content management system"
+    ),
+    (
+        "crm",
+        "software",
+        "customer relations manager"
+    ),
+    (
+        "xampp",
+        "software",
+        "webserver manager"
+    ),
+    (
+        "mariadb",
+        "software",
+        "webserver database"
+    ),
+    (
+        "juice_shop",
+        "software",
+        "E-commerce website"
+    ),
+    (
+        "customer_data",
+        "data",
+        "data associated with individual customers"
+    ),
+    (
+        "employee_data",
+        "data",
+        "data associated with employees"
+    ),
+    (
+        "sales_data",
+        "data",
+        "data associated with transactions, payment status, shipping status"
+    ),
+    (
+        "branding_assets",
+        "data",
+        "standard branding info (logos, watermarks, templates, style sheets)"
+    ),
+    (
+        "owner",
+        "people",
+        "manages operation, makes HR decisions"
+    ),
+    (
+        "csr",
+        "people",
+        "answers questions, provides support, resolves complaints"
+    ),
+    (
+        "csr",
+        "people",
+        "answers questions, provides support, resolves complaints"
+    ),
+    (
+        "it",
+        "people",
+        "manages technology, responds to IT issues"
+    ),
+    (
+        "sales",
+        "people",
+        "answers calls, manages customer accounts, evaluates and meets customer needs"
+    ),
+    (
+        "payment_process",
+        "processes",
+        "direct customers to 3rd party payment portal"
+    ),
+    (
+        "sales",
+        "processes",
+        "use phones for inbound and certain outbound sales calls"
+    ),
+    (
+        "customer_support",
+        "processes",
+        "use phones and online chat to provide customer service"
+    ),
+    (
+        "inventory_management",
+        "processes",
+        "use inventory workstation it manager and owner verify inventory regularly"
+    )
